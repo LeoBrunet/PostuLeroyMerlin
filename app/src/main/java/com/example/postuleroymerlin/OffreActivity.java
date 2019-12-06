9c22cec2333bf9839318d8fa0bfa6209a6f81b6a
Simon <dubois.simon19@gmail.com>
2019-12-06 06:05:06 +0100

    parser offre detail

--- app/src/main/java/com/example/postuleroymerlin/OffreActivity.java
+++ app/src/main/java/com/example/postuleroymerlin/OffreActivity.java
@@ -9,6 +9,22 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.postuleroymerlin.Model.OffreDetails;
+import com.example.postuleroymerlin.Model.OffrePreface;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 public class OffreActivity extends AppCompatActivity {
 
     private TextView postuler;
@@ -27,5 +43,40 @@ public class OffreActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private OffreDetails parserXMLOnOneElement() throws IOException, SAXException, ParserConfigurationException {
+        OffreDetails offre = null;
+
+        int position = getIntent().getIntExtra("position", -1);
+
+        InputStream inputFile = getResources().getAssets().open("offre.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+
+        Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+        NodeList list = doc.getElementsByTagName("offre");
+
+        for(int i = 0; i < list.getLength(); i++) {
+            if(i == position){
+                Node node = list.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    String title = eElement.getElementsByTagName("title").item(0).getTextContent();
+                    String contrat = eElement.getElementsByTagName("contrat").item(0).getTextContent();
+                    String lieu = eElement.getElementsByTagName("lieu").item(0).getTextContent();
+                    String experience = eElement.getElementsByTagName("experience").item(0).getTextContent();
+                    String filiere = eElement.getElementsByTagName("filiere").item(0).getTextContent();
+                    String publication = eElement.getElementsByTagName("publication").item(0).getTextContent();
+                    String p1 = eElement.getElementsByTagName("p1").item(0).getTextContent();
+                    String p2 = eElement.getElementsByTagName("p2").item(0).getTextContent();
+                    String profil = eElement.getElementsByTagName("profil").item(0).getTextContent();
+                    String lien = eElement.getElementsByTagName("lien").item(0).getTextContent();
+                    offre = new OffreDetails(new OffrePreface(title, contrat, lieu, experience, filiere, publication), p1, p2, profil, lien);
+                }
+            }
+        }
+        return offre;
     }
 }
