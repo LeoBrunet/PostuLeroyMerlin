0a5014998d2631761c9d351c016fe3487f967e9e
Leo Brunet <leobrunet91@gmail.com>
2019-12-06 02:36:45 +0100

    annonce2

--- app/src/main/java/com/example/postuleroymerlin/MainActivity.java
+++ app/src/main/java/com/example/postuleroymerlin/MainActivity.java
@@ -26,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        btnQuestions = findViewById(R.id.btnQuestions);
+        loadFragement(new OffreFragment());
+
+        //btnQuestions = findViewById(R.id.btnQuestions);
 
         spaceNavigationView = (SpaceNavigationView) findViewById(R.id.space);
         spaceNavigationView.initWithSaveInstanceState(savedInstanceState);
@@ -60,13 +62,13 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        btnQuestions.setOnClickListener(new View.OnClickListener() {
+        /*btnQuestions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),Questions.class);
                 startActivity(intent);
             }
-        });
+        });*/
 
 
     }
